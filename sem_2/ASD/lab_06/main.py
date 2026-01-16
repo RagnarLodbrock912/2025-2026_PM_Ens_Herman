3e3d66c9e5f48c5fa27a2ba61fa245c284325a1c
RagnarLodbrock912 <137827970+RagnarLodbrock912@users.noreply.github.com>
2026-01-16 21:11:16 +0200

    Add lab_06 ASD sem_2

---
+++ sem_2/ASD/lab_06/main.py
@@ -0,0 +1,24 @@
+def rk(text, pattern):
+    k = 0
+    flag = False
+
+    while k < len(text) - len(pattern) + 1 and not flag:
+        a = text[k:k + len(pattern)]
+        if hash(pattern) != hash(text[k:k + len(pattern)]):
+            k += 1
+            continue
+
+        flag = True
+
+        for i in range(len(pattern)):
+            if pattern[i] != text[k + i]:
+                flag = False
+                break
+    
+    return flag
+
+print(rk("abcd", "abcd"))  # True
+print(rk("zzabcdzz", "abcd"))  # True
+print(rk("abxabc", "abcd"))  # False
+
+        
