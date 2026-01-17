ab146362a1b9feb2de74eef7b23455d04d73e168
RagnarLodbrock912 <137827970+RagnarLodbrock912@users.noreply.github.com>
2026-01-17 15:27:29 +0200

    Add lab_10 ASD sem_2

---
+++ sem_2/ASD/lab_10/main.py
@@ -0,0 +1,17 @@
+def eggs(n=100, e=2):
+    dp = [[0] * (n + 1) for _ in range(e + 1)]
+
+    for i in range(1, e + 1):
+        dp[i][1] = 1
+
+    for j in range(1, n + 1):
+        dp[1][j] = j
+    for i in range(2, e + 1):
+        for j in range(2, n + 1):
+            dp[i][j] = dp[i-1][j-1] + dp[i][j-1] + 1
+
+    for i, el in enumerate(dp[e]):
+        if el >= n:
+            return i
+
+print(eggs())
